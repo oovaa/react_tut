d1d30aa03e55e22d138eab4c3eeb86ef0aaba268
oovaa <omarvx211@gmail.com>
2024-12-18 15:28:34 +0200

    working calc with a double op bug fixed line  37 im the GOAT

--- wds_calc/src/App.js
+++ wds_calc/src/App.js
@@ -1,8 +1,7 @@
-import React, { useReducer } from 'react'
-import './App.css';
-import DigitbButton from './DigitbButton'
-import OperationButtton from './OperationButtton';
-
+import React, { useReducer } from "react";
+import "./App.css";
+import DigitbButton from "./DigitbButton";
+import OperationButtton from "./OperationButtton";
 
 export const ACTIONS = {
   ADD_DIGIT: "add-digit",
@@ -10,40 +9,96 @@ export const ACTIONS = {
   CLEAR: "clear",
   DELETE: "delete",
   EVALUATE: "evaluate",
-}
+};
 
 const reducer = (state, { type, payload }) => {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
-      if (payload.digit === '0' && state.cur === '0')
-        return state
+      if (payload.digit === "0" && state.cur === "0") return state;
 
-      if (payload.digit === '.' && (state.cur || "").includes('.'))
-        return state
+      if (payload.digit === "." && (state.cur || "").includes("."))
+        return state;
 
       return {
         ...state,
-        cur: `${state.cur || ""}${payload.digit}`
+        cur: `${state.cur || ""}${payload.digit}`,
+      };
+    case ACTIONS.CHOOSE_OP:
+      if (state.cur == null && state.prev == null) return state;
+
+      if (state.prev == null) {
+        return {
+          ...state,
+          op: payload.Operation,
+          prev: state.cur,
+          cur: null,
+        };
       }
+      if (state.prev != null && state.op != null && state.cur == null)
+        return state;
+
+      return {
+        ...state,
+        prev: evaluate(state.prev, state.cur, state.op),
+        op: payload.Operation,
+        cur: null,
+      };
+
+    case ACTIONS.CLEAR:
+      return {};
+    case ACTIONS.DELETE:
 
     default:
       break;
   }
+};
 
+function evaluate(current, previous, op) {
+  const cur = parseFloat(current);
+  const prev = parseFloat(previous);
+  if (isNaN(prev) || isNaN(cur)) {
+    return "";
+  }
+  let result = "";
+
+  switch (op) {
+    case "+":
+      result = cur + prev;
+      break;
+    case "*":
+      result = cur * prev;
+      break;
+    case "-":
+      result = cur - prev;
+      break;
+    case "÷":
+      result = cur / prev;
+      break;
+    default:
+      return "";
+  }
+  return result.toString();
 }
 
 function App() {
-
-  const [{ cur, prev, op }, dispatch] = useReducer(reducer, {})
+  const [{ cur, prev, op }, dispatch] = useReducer(reducer, {});
 
   return (
-    <div className='calculator-gird'>
-      <div className='output'>
-        <div className='prev-operand'>{prev} {op}</div>
-        <div className='cur-operand'>{cur} </div>
+    <div className="calculator-gird">
+      <div className="output">
+        <div className="prev-operand">
+          {prev}
+          {op}
+        </div>
+        <div className="cur-operand">{cur} </div>
       </div>
-      <button className='span-two'>AC</button>
-      <button>DEL</button>
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+      >
+        AC
+      </button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE })}>DEL</button>
       <OperationButtton Operation="÷" dispatch={dispatch} />
       <DigitbButton digit="1" dispatch={dispatch} />
       <DigitbButton digit="2" dispatch={dispatch} />
@@ -59,9 +114,9 @@ function App() {
       <OperationButtton Operation="-" dispatch={dispatch} />
       <DigitbButton digit="." dispatch={dispatch} />
       <DigitbButton digit="0" dispatch={dispatch} />
-      <button className='span-two'>=</button>
+      <button className="span-two">=</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
